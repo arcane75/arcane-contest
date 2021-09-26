51f9cb2f35d5c1737267aeac46dad4155ebf0a45
unknown <mdrakibulhoque83@gmail.com>
2021-09-26 18:28:28 +0600

    read.me file update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,19 +4,16 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = (props) => {
-    const {cart} = props;
+    const { cart } = props;
 
-    //let totalQuantity = 0;
-    let grandTotal = 0 ;
+    let grandTotal = 0;
     const totalReducer = (previous, university) => previous + university.teamfee;
     grandTotal = cart.reduce(totalReducer, 0);
 
-    //const quantityReducer = (previous, university) => previous + university.quantity;
-    //totalQuantity = cart.reduce(quantityReducer,0);
 
     return (
         <div className="cart-style">
-            <h3><FontAwesomeIcon icon = {faPeopleArrows} /> Team Participate Summary</h3>
+            <h3><FontAwesomeIcon icon={faPeopleArrows} /> Team Participate Summary</h3>
             <h5>Team Participate: {props.cart.length}</h5>
             <p>Team Fee: {"4000"}</p>
             <p>Total Fee Collected: {grandTotal}</p>
