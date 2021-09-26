01379e7e535bc7b8f1bf80faa122193f88178560
unknown <mdrakibulhoque83@gmail.com>
2021-09-26 20:49:03 +0600

    name shown

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,13 +10,30 @@ const Cart = (props) => {
     const totalReducer = (previous, university) => previous + university.teamfee;
     grandTotal = cart.reduce(totalReducer, 0);
 
+    let add = [];
 
+    for (const university of cart) {
+
+        add += university.name + " ";
+        
+
+    }
     return (
-        <div className="cart-style">
-            <h3><FontAwesomeIcon icon={faPeopleArrows} /> Team Participate Summary</h3>
-            <h5>Team Participate: {props.cart.length}</h5>
-            <p>Team Fee: {"4000"}</p>
-            <p>Total Fee Collected: {grandTotal}</p>
+        <div>
+            <div className="cart-style">
+                <h3><FontAwesomeIcon icon={faPeopleArrows} /> Team Participate Summary</h3>
+                <h5>Team Participate: {props.cart.length}</h5>
+                <p>Team Fee: {"4000"}</p>
+                <p>Total Fee Collected: {grandTotal}</p>
+            </div>
+
+            <div className="added-participate">
+                <h3> Added Participate </h3>
+                <div className="participate">
+                    <h4> { add } </h4>
+                </div>
+            </div>
+
         </div>
     );
 };
